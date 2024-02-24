d1edaf46246ec73651687166fd671394b1c5af00
kaan <kabuke13@gmail.com>
2024-02-24 15:58:22 +0000

    lab changes

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,28 @@
 class Tree:
-  def __init__(self, root = None):
+  def __init__(self, root=None):
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    result = None
+    nodes_to_visit = [self.root]
+
+    while len(nodes_to_visit) > 0:
+      current_node = nodes_to_visit.pop(0)
+
+      if isinstance(id, str) and id == current_node["id"]:
+        result = current_node
+        return result
+
+      nodes_to_visit.extend(current_node["children"])
+
+    return None
+    
+    
+
+
+        
+
+        
+
+
+
